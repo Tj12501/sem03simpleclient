c2388a9fa06fbf014692e37d74078a35356d736f
Tj12501 <thomas.john.frivold@gmail.com>
2023-05-03 08:17:20 +0000

    Pusher for at LA skal se p

--- tcp_client.go
+++ tcp_client.go
@@ -4,17 +4,20 @@ import (
 	"net"
 	"log"
 	"os"
+        "github.com/Tj12501/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.5:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+ 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+        log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,5 +27,8 @@ func main() {
 		log.Fatal(err)
 	} 
 	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	dekryptertMelding := mycrypt.Krypter([]rune(response), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Printf("reply from proxy: %s", dekryptertMelding)
 }
+
+
